fa6d0081891ee33d9fd51f0b6dc8ddadcd196856
yskc0315 <ihcikihsoy1503@gmail.com>
2021-04-08 13:29:29 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :genres, only:[:index, :create]
   resources :notifications, only:[:index]
 
+
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
